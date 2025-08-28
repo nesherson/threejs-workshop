5861f8907bec2359d488c49964fc3b7f41fb7ca4
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-08-28 22:38:53 +0200

    Create lesson folders, add fourth lesson

--- src/lessons/lesson-03/script.js
+++ src/lessons/lesson-03/script.js
@@ -9,20 +9,13 @@ const canvas = document.querySelector(".canvas");
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
 const scene = new THREE.Scene();
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const boxGeometry = new THREE.BoxGeometry(2, 2, 2);
 const boxMaterial = new THREE.MeshBasicMaterial({ color: "red" });
 const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
 
-camera.position.z = 10;
-
-boxMesh.position.set(4, -4, 0);
-boxMesh.scale.set(1, 1, 5);
-boxMesh.rotation.y = Math.PI * 0.25;
-boxMesh.rotation.x = Math.PI * 0.25;
-
+camera.position.z = 3;
 
 renderer.setSize(size.width, size.height);
 scene.add(boxMesh);
 scene.add(camera);
-scene.add(new THREE.AxesHelper());
 renderer.render(scene, camera);
