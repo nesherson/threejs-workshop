14ba874959d155fcdc5d7d20851cfa841cd5af9b
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-08-31 20:45:36 +0200

    Update lesson six

--- src/lessons/lesson-06/script.js
+++ src/lessons/lesson-06/script.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 const size = {
     width: 800,
@@ -8,28 +9,21 @@ const size = {
 const canvas = document.querySelector(".canvas");
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
+const orbitControls = new OrbitControls(camera, canvas);
 const scene = new THREE.Scene();
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 const boxMaterial = new THREE.MeshBasicMaterial({ color: "red" });
 const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
 
 camera.position.z = 5;
+orbitControls.enableDamping = true;
 
 renderer.setSize(size.width, size.height);
 scene.add(boxMesh);
 scene.add(camera);
 
-const clock = new THREE.Clock();
-
 function tick() {
-
-    const elapsedTime = clock.getElapsedTime();
-
-    console.log(elapsedTime);
-
-    boxMesh.position.x = Math.sin(elapsedTime);
-    boxMesh.position.y = Math.cos(elapsedTime);
-    camera.lookAt(boxMesh.position);
+    orbitControls.update();
     renderer.render(scene, camera);
     window.requestAnimationFrame(tick);
 }
