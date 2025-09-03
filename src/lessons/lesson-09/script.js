d9262a47148a624a94d82e0e2d25935d1918d5d8
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-09-03 23:01:33 +0200

    Finish ninth lesson

--- src/lessons/lesson-09/script.js
+++ src/lessons/lesson-09/script.js
@@ -1,5 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
+import GUI from "lil-gui";
+import gsap from "gsap";
 
 window.addEventListener("resize", (e) => {
     size.width = window.innerWidth;
@@ -20,37 +22,64 @@ window.addEventListener("dblclick", (e) => {
     }
 });
 
+window.addEventListener("keydown", (e) => {
+    if (e.key.toLowerCase() === "h") {
+        gui.show(gui._hidden);
+    }
+})
+
+const gui = new GUI({
+    title: "Debug UI",
+    closeFolders: true
+});
+const debugObject = {};
+
 const size = {
     width: window.innerWidth,
     height: window.innerHeight
 }
+debugObject.color = "#5e4555";
+debugObject.spin = () => {
+    gsap.to(boxMesh.rotation, { duration: 1, y: boxMesh.rotation.y + Math.PI * 2});
+}
+debugObject.subdivision = 2;
 
 const canvas = document.querySelector(".canvas");
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
 const orbitControls = new OrbitControls(camera, canvas);
 const scene = new THREE.Scene();
-const geometry = new THREE.BufferGeometry();
-const count = 25;
-const positions = new Float32Array(count * 3 * 3);
-
-for (let i = 0; i < count * 3 * 3; i++) {
-    positions[i] = (Math.random() - 0.5) * 5;
-}
-
-const positionsAttribute = new THREE.BufferAttribute(positions, 3);
-
-geometry.setAttribute("position", positionsAttribute);
-
-const geometryMaterial = new THREE.MeshBasicMaterial({ color: "red", wireframe: true });
-const geometryMesh = new THREE.Mesh(geometry, geometryMaterial);
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
+const boxMaterial = new THREE.MeshBasicMaterial({ color: debugObject.color, wireframe: true });
+const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
 
 camera.position.z = 5;
 orbitControls.enableDamping = true;
 
 renderer.setSize(size.width, size.height);
-scene.add(geometryMesh);
+scene.add(boxMesh);
 scene.add(camera);
+
+const boxTweaks = gui.addFolder("Box");
+
+boxTweaks.add(boxMesh.position, "y").name("Y").min(-3).max(3).step(0.01);
+boxTweaks.add(boxMesh.position, "x").name("X").min(-3).max(3).step(0.01);
+boxTweaks.add(boxMesh, "visible");
+boxTweaks.add(boxMaterial, "wireframe");
+boxTweaks.addColor(debugObject, "color")
+   .onChange(() => {
+    boxMaterial.color.set(debugObject.color);
+})
+boxTweaks.add(debugObject, "spin");
+boxTweaks.add(debugObject, "subdivision")
+   .min(1)
+   .max(10)
+   .step(1)
+   .onFinishChange(() => {
+    boxMesh.geometry.dispose();
+    boxMesh.geometry = 
+        new THREE.BoxGeometry(1, 1, 1, debugObject.subdivision, debugObject.subdivision, debugObject.subdivision);
+   });
 
 function tick() {
     orbitControls.update();
--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 import GUI from "lil-gui";
+import gsap from "gsap";
 
 window.addEventListener("resize", (e) => {
     size.width = window.innerWidth;
@@ -21,12 +22,27 @@ window.addEventListener("dblclick", (e) => {
     }
 });
 
-const gui = new GUI();
+window.addEventListener("keydown", (e) => {
+    if (e.key.toLowerCase() === "h") {
+        gui.show(gui._hidden);
+    }
+})
+
+const gui = new GUI({
+    title: "Debug UI",
+    closeFolders: true
+});
+const debugObject = {};
 
 const size = {
     width: window.innerWidth,
     height: window.innerHeight
 }
+debugObject.color = "#5e4555";
+debugObject.spin = () => {
+    gsap.to(boxMesh.rotation, { duration: 1, y: boxMesh.rotation.y + Math.PI * 2});
+}
+debugObject.subdivision = 2;
 
 const canvas = document.querySelector(".canvas");
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
@@ -34,7 +50,7 @@ const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
 const orbitControls = new OrbitControls(camera, canvas);
 const scene = new THREE.Scene();
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
-const boxMaterial = new THREE.MeshBasicMaterial({ color: "red", wireframe: true });
+const boxMaterial = new THREE.MeshBasicMaterial({ color: debugObject.color, wireframe: true });
 const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
 
 camera.position.z = 5;
@@ -44,11 +60,26 @@ renderer.setSize(size.width, size.height);
 scene.add(boxMesh);
 scene.add(camera);
 
-gui.add(boxMesh.position, "y").name("Y").min(-3).max(3).step(0.01);
-gui.add(boxMesh.position, "x").name("X").min(-3).max(3).step(0.01);
-gui.add(boxMesh, "visible");
-gui.add(boxMaterial, "wireframe");
-gui.addColor(boxMaterial, "color")
+const boxTweaks = gui.addFolder("Box");
+
+boxTweaks.add(boxMesh.position, "y").name("Y").min(-3).max(3).step(0.01);
+boxTweaks.add(boxMesh.position, "x").name("X").min(-3).max(3).step(0.01);
+boxTweaks.add(boxMesh, "visible");
+boxTweaks.add(boxMaterial, "wireframe");
+boxTweaks.addColor(debugObject, "color")
+   .onChange(() => {
+    boxMaterial.color.set(debugObject.color);
+})
+boxTweaks.add(debugObject, "spin");
+boxTweaks.add(debugObject, "subdivision")
+   .min(1)
+   .max(10)
+   .step(1)
+   .onFinishChange(() => {
+    boxMesh.geometry.dispose();
+    boxMesh.geometry = 
+        new THREE.BoxGeometry(1, 1, 1, debugObject.subdivision, debugObject.subdivision, debugObject.subdivision);
+   });
 
 function tick() {
     orbitControls.update();
